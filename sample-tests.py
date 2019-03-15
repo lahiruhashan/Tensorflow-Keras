045ebd27b369542ec0ac5cf780bb89659200f99f
hashan <hashanlahiru6@gmail.com>
2019-03-15 06:02:42 +0530

    reshaping in numpy

--- sample-tests.py
+++ sample-tests.py
@@ -1,6 +1,7 @@
 from keras.datasets import mnist
 import tensorflow as tf
 import numpy as np
+from keras import layers
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
@@ -14,8 +15,20 @@ y = np.random.random((32, 10))
 
 z = np.maximum(x, y)
 
-print(z.shape)
 
+l = x[0:, 0:, 0, 0]
+print(l.shape)
+
+
+# reshaping
+n = np.array([[0., 1.],
+              [2., 3.],
+              [4., 5.]])
+
+print(n.shape)
+print(n.reshape(6, 1))
+print(n.reshape(2, 3))
+print(np.transpose(n))
 
 
 
