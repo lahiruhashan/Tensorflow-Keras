093d9cc54fd2cbd9ab7f853b0d1340ec4bb3e7e3
hashan <hashanlahiru6@gmail.com>
2019-03-16 11:25:06 +0530

    testing with different optimizing approaches

--- keras-for-first-time.py
+++ keras-for-first-time.py
@@ -2,6 +2,7 @@ from keras.datasets import mnist
 from keras import models
 from keras import layers
 from keras.utils import to_categorical
+from keras import optimizers
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 network = models.Sequential()
@@ -9,6 +10,7 @@ network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 network.add(layers.Dense(10, activation='softmax'))
 
 network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+# network.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='mse', metrics=['accuracy'])
 
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32') / 255
@@ -22,4 +24,4 @@ test_labels = to_categorical(test_labels)
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
 
 test_loss, test_acc = network.evaluate(test_images, test_labels)
-print('Accuracy : ', test_acc)
+print('Accuracy : ', test_acc * 100, '%')
